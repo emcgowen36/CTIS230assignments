3d22cbe1ef5614a9ebb3a9d5a60d6563000e843d
emcgowen36 <emcgowen@guilford.edu>
2024-03-12 01:53:54 -0400

    General Assignment 6

---
+++ testscript.js
@@ -0,0 +1,25 @@
+myitem = document.getElementById("firsttest");
+myitem.addEventListener("click", onClick);
+function onClick() {  
+    myitem.style.color = "red";  
+    myitem.style.fontSize = "36px";
+}
+function onmouseout() {
+    myitem.style.color = "rgb(11, 11, 112)";
+    myitem.style.fontSize = "small";
+}
+//first line is grabbing the text from the html file
+//second line is what I will do to the line
+//third line is what will happen to the line when I commit the action
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {  
+    otheritem.style.color = "red";  
+}
+textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
