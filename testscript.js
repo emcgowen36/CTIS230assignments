2f6ff69af300a34f0c2b5e9c850c26d665263a9f
emcgowen36 <emcgowen@guilford.edu>
2024-03-18 16:13:58 -0400

    General Assignment 6 Update

--- testscript.js
+++ testscript.js
@@ -1,12 +1,13 @@
 myitem = document.getElementById("firsttest");
-myitem.addEventListener("click", onClick);
-function onClick() {  
-    myitem.style.color = "red";  
-    myitem.style.fontSize = "36px";
+    myitem.addEventListener("click", onClick);
+    function onClick() {  
+        myitem.style.color = "red";  
+        myitem.style.fontSize = "36px";
 }
-function onmouseout() {
-    myitem.style.color = "rgb(11, 11, 112)";
-    myitem.style.fontSize = "small";
+    myitem.addEventListener("click", onMouseOut);
+    function onMouseOut() {
+        myitem.style.color = "rgb(11, 11, 112)";
+        myitem.style.fontSize = "small";
 }
 //first line is grabbing the text from the html file
 //second line is what I will do to the line
